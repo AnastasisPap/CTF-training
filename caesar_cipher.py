c4d82f5e7c6ac01f667b16cc70f66f5925ea463a
Anastasispap <anastasis.papapanagiotou@gmail.com>
2021-02-25 21:54:58 +0200

    Added encoding with caesar cipher

--- caesar_cipher.py
+++ caesar_cipher.py
@@ -1,14 +1,40 @@
-code = input("Enter text to decipher: ")
-code = code.split()
 letters = [chr(i+ord('a')) for i in range(26)]
+def decode():
+    code = input("Enter text to decipher: ").split()
 
 
-for i in range(1, 27):
+    for i in range(1, 27):
+        s = ''
+        for item in code:
+            for char in item:
+                idx = letters.index(char)
+                s += letters[(idx+i)%26]
+            s += ' '
+        print(f"Key {i}: {s}")
+    
+
+
+
+def encode():
+    code = input("Enter text to encode: ").split()
+    shifts = int(input("Enter amount of shifts 1-26: "))
+    
     s = ''
     for item in code:
         for char in item:
             idx = letters.index(char)
-            s += letters[(idx+i)%26]
+            s += letters[(idx-shifts+26)%26]
         s += ' '
-    print(f"Key {i}: {s}")
-    
+    print(f"Encoded text: {s}")
+
+
+def main():
+    choice = int(input("Type 1 to encode or 2 to decode: "))
+    if choice == 1:
+        encode()
+    else:
+        decode()
+
+
+if __name__ == '__main__':
+    main()
